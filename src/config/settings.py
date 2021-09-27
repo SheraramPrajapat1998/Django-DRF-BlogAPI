866e002fd6f5a8c7a93f28a47ddfda6ce19214ad
SheraramPrajapat1998 <sheraramprajapat1998@gmail.com>
2021-09-27 20:01:11 +0530

    posts app added

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local apps
+    'posts.apps.PostsConfig',
+
+
+    # Third-party apps
+    'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
